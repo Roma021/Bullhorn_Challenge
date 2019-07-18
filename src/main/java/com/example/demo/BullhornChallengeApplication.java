f119fcaf7368a3f789eb6d6d9d81126f6b3f2d69
Roma021 <dtmogh@gmail.com>
2019-07-18 12:08:58 -0400

    Bullhorn Challenge

---
+++ src/main/java/com/example/demo/BullhornChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullhornChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullhornChallengeApplication.class, args);
+	}
+
+}
